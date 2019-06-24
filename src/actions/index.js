7d7e27044733845b86bb69ceb7736c47760df1a4
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2019-06-24 09:54:05 -0700

    started the actions tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
-import {types} from './types';
+import axios from 'axios';
+import types from './types';
+
+export const fetchPosts = () => async (dispatch) => {
+	await axios.get('https://test.wineapp.me/api/v1/wines')
+		.then(res => {
+			dispatch({
+				type: types.GET_WINES,
+				payload:  red.data
+			})
+		})
+		.catch(err => {
+			consoe.log(err);
+		})
+};
