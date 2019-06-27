3c12904497acfe9e92d718522ca4fcdd9413908b
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2019-06-27 10:18:07 -0700

    Com,pleted pp

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import './App.scss';
 import Header from './components/Header';
 import Headline from './components/Headline';
 import ListItem from './components/ListItem';
-import Button from './components/button';
 import {connect}  from 'react-redux';
 import {fetchWines} from './store/actions';
+import Button from '@material-ui/core/Button';
+
+// Card importsw
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 const tempArr = [{
   name: 'Eben',
   age: 24,
   isTall: false
 }]
+
+const useStyles = makeStyles({
+  card: {
+    width: 300
+  }
+})
 
 class App extends React.Component {
 
@@ -26,13 +41,46 @@ class App extends React.Component {
     }
   
     render() {
-        console.log(this.props);
+
+      console.log(this.props);
         return (
           <div>
             <Header />
             <section className="main">
               <Headline header="Wines" tempArr={tempArr} desc="Click the button to render wines!"/>
-              <Button  buttonText="Get Wines" emitEvent = {() => this.fetchWines()} />
+              <Button variant="contained" color="primary" onClick={() => this.fetchWines()}>
+                Load Wines
+              </Button>
+
+              <div style={{marginTop: '50px'}}>
+                 <Card style={{width: '300px'}}>
+                  <CardActionArea>
+                    <CardMedia
+                      style={{height: '200'}}
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ6da-FK-uZjxTxYYNGSbKi0cumc1CoHS673iHweXh1laMakXG"
+                      title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Lizard
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Share
+                    </Button>
+                    <Button size="small" color="primary">
+                      Learn More
+                    </Button>
+                  </CardActions>
+                </Card>
+              </div>
+             
             </section>
             
           </div>
